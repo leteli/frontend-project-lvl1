1ef52f8cca6b040b1626a3545b438224c5756ab7
Nastia Kartseva <danetzachem@gmail.com>
2021-10-31 20:08:57 +0300

    improve code style in src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
-export const greetUser = () => {
-let userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+
+const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+export default greetUser;
